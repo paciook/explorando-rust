490c99a945298e83d7b39a4aef2f1d5474ebaf71
Francisco Pacio <fpaciook@gmail.com>
2020-09-09 22:55:01 -0300

    Primer Hola mundo

---
+++ helloworld/target/debug/deps/helloworld-34a048274b01aae9.d
@@ -0,0 +1,5 @@
+/mnt/d/Docs/Rust/explorando-rust/helloworld/target/debug/deps/helloworld-34a048274b01aae9: src/main.rs
+
+/mnt/d/Docs/Rust/explorando-rust/helloworld/target/debug/deps/helloworld-34a048274b01aae9.d: src/main.rs
+
+src/main.rs:
---
+++ helloworld/target/debug/helloworld.d
@@ -0,0 +1,1 @@
+/mnt/d/Docs/Rust/explorando-rust/helloworld/target/debug/helloworld: /mnt/d/Docs/Rust/explorando-rust/helloworld/src/main.rs
